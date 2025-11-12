783e27eebec916d952f76a417543e5234e96d6f5
FedericoCanale <icocanale@gmail.com>
2025-11-12 12:50:49 +0100

    Add card with required infos, no style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Main() {
-    useEffect(() => {
-        axios.get("https://lanciweb.github.io/demo/api/actresses/")
-            .then(res => console.log(res.data))
-            .catch(err => console.error(err));
+    const [acts, setActs] = useState([]);
+
+    useEffect(() => { // add useEfffect to catch the api contenent 
+        axios
+            .get("https://lanciweb.github.io/demo/api/actresses/")
+            .then((res) => setActs(res.data))
+            .catch((err) => console.error(err));
     }, []);
 
-    return
+    return ( // Starting to create the card with required infos
+        <div>
+            {acts.map((act, index) => (
+                <div key={index}>
+                    <h3>{act.name}</h3>
+                    <img src={act.image} alt={act.name} width="150" />
+                    <p>Year of birth: {act.birth_year}</p>
+                    <p>Nationality: {act.nationality}</p>
+                    <p>Biography: {act.biography}</p>
+                    <p>Awards: {act.awards}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
