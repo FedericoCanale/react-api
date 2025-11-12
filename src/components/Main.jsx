c9e07d3816318dc0b2f71b908f708559999ff00c
FedericoCanale <icocanale@gmail.com>
2025-11-12 14:29:55 +0100

    Add two list for both actor and actress

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,24 +13,19 @@ export default function Main() {
         // Chiamata API per le attrici
         axios
             .get(actressApi)
-            .then((res) => {
-                console.log(res.data);
-                setActresses(res.data);
-            })
+            .then((res) => setActresses(res.data))
             .catch((err) => console.error(err));
 
         // Chiamata API per gli attori
         axios
-            .get(actressApi)
-            .then((res) => {
-                console.log(res.data);
-                setActresses(res.data);
-            })
+            .get(actorApi)
+            .then((res) => setActors(res.data))
             .catch((err) => console.error(err));
     }, []);
 
     return (
         <div className="container my-4">
+            {/* Lista Attrici */}
             <h1 className="text-center mb-4">Lista Attrici</h1>
             <div className="row">
                 {actresses.map((act, index) => (
@@ -41,7 +36,30 @@ export default function Main() {
                                 alt={act.name}
                                 className="card-img-top actress-img"
                             />
-                            <div className="card-body">
+                            <div className="card-body-actress">
+                                <h5 className="card-title">{act.name}</h5>
+                                <p className="card-text">Anno di nascita: {act.birth_year}</p>
+                                <p className="card-text">Nazionalità: {act.nationality}</p>
+                                <p className="card-text">Biografia: {act.biography}</p>
+                                <p className="card-text">Riconoscimenti: {act.awards}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* Lista Attori */}
+            <h1 className="text-center mb-4 mt-5">Lista Attori</h1>
+            <div className="row">
+                {actors.map((act, index) => (
+                    <div key={index} className="col-md-4 mb-4">
+                        <div className="card actor-card h-100 text-center">
+                            <img
+                                src={act.image}
+                                alt={act.name}
+                                className="card-img-top actor-img"
+                            />
+                            <div className="card-body-actor">
                                 <h5 className="card-title">{act.name}</h5>
                                 <p className="card-text">Anno di nascita: {act.birth_year}</p>
                                 <p className="card-text">Nazionalità: {act.nationality}</p>
